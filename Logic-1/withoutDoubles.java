97487615b77b0f3387e6e813dbbfe1d12b1c7c26
yousuf.shaaili <alshaili37@gmail.com>
2025-07-31 08:10:05 +0400

    Add solution for logic1-withoutDoubles

---
+++ Logic-1/withoutDoubles.java
@@ -0,0 +1,7 @@
+public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+  if (noDoubles &&die1==die2) {
+        if (die1== 6)die1= 1;
+        else die1++;
+    }
+    return die1 + die2;
+}
